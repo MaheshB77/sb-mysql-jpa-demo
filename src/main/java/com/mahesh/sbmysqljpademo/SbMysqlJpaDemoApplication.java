137b48cbf37990304c79eea011e7a4828921f537
MaheshB77 <mbansode7696@gmail.com>
2021-09-12 18:08:52 +0530

    Initial commit

---
+++ src/main/java/com/mahesh/sbmysqljpademo/SbMysqlJpaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.sbmysqljpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMysqlJpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMysqlJpaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahesh/sbmysqljpademo/SbMysqlJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.sbmysqljpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMysqlJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
